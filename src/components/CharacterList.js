bc32e3c0ba9ebb0709c2d048e09fc0cd6c92cae4
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-23 09:22:16 -0700

    working on locationlist

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,24 +1,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const [character, SetCharacter] = useState({})
+  const [characterState, SetCharacter] = useState({})
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios
-    .post("https://rickandmortyapi.com/api/character/")
+    .get("https://rickandmortyapi.com/api/character/")
     .then(res => {
       console.log(res.data.results);
       SetCharacter(res.data.results);
     })
     .catch(err => console.log(err.response));
   }, []);
+  
+  if(!characterState)return <h3>loading...</h3>;
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characterState.map((char, index) => {
+        return(
+          <CharacterCard key={index} allData={char} />
+        )
+      })}
     </section>
   );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,28 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import LocationCard from "./LocationCard"; 
 
-export default function LocationsList() {}
+export default function LocationsList() {
+    const [locationState, SetLocation] = useState({})
+    useEffect(() => {
+      axios
+      .get("https://rickandmortyapi.com/api/location/")
+      .then(res => {
+        console.log(res.data.results);
+        SetCharacter(res.data.results);
+      })
+      .catch(err => console.log(err.response));
+    }, []);
+    
+    if(!LocationState)return <h3>loading...</h3>;
+  
+    return (
+      <section className="location-list grid-view">
+        {locationState.map((loc) => {
+          return(
+            <LocationCard key={loc.id} loc={loc} />
+          )
+        })}
+      </section>
+    );
+}
