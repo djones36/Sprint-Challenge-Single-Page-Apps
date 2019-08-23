01a22507d998f1b5e761b03343f213a90581c77f
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-23 12:05:15 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-// import CharacterList from './components/CharacterList';
-// import LocationsList from './components/LocationsList';
-// import EpisodeList from './components/EpisodeList';
+import CharacterList from './components/CharacterList';
+import WelcomePage from "./components/WelcomePage";
+import {Route} from "react-router-dom";
+import LocationsList from './components/LocationsList';
+import EpisodeList from './components/EpisodeList';
 
 export default function App() {
   
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Tab, Menu} from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Route } from "react-router-dom";
 import WelcomePage from "./WelcomePage";
 import CharacterList from './CharacterList';
 import LocationsList from './LocationsList';
@@ -9,18 +9,35 @@ import EpisodeList from './EpisodeList';
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
-// export default function TabNav() {
+export default function TabNav() {
+  return(
+    <div>
+    <nav>
+      <NavLink exact to="/" activeClassName="nav-active">Home</NavLink>
+      <NavLink exact to="/characters" activeClassName="nav-active">Characters</NavLink>
+      <NavLink exact to="/location" activeClassName="nav-active">Locations</NavLink>
+      <NavLink exact to="/episodes" activeClassName="nav-active">Episodes</NavLink>
+    </nav>
+    <div>
+      <Route exact path="/" component={WelcomePage}/>
+      <Route exact path="/characters" component={CharacterList} />
+      <Route exact path="/location" component={LocationsList} />
+      <Route exact path="/episodes" component={EpisodeList} /> 
+    </div>
+  </div>
 
 
-// };
+  )
 
-const panes = [
-    { menuItem: 'Home', render: () => <Tab.Pane><WelcomePage/></Tab.Pane> },
-    { menuItem: 'Characters', render: () => <Tab.Pane><CharacterList/></Tab.Pane> },
-    { menuItem: 'Location', render: () => <Tab.Pane><LocationsList/></Tab.Pane> },
-    { menuItem: 'Episode', render: () => <Tab.Pane><EpisodeList/></Tab.Pane> },
-  ]
+};
+
+// const panes = [
+//     { menuItem: 'Home', render: () => <Tab.Pane><WelcomePage/></Tab.Pane> },
+//     { menuItem: 'Characters', render: () => <Tab.Pane><CharacterList/></Tab.Pane> },
+//     { menuItem: 'Location', render: () => <Tab.Pane><LocationsList/></Tab.Pane> },
+//     { menuItem: 'Episode', render: () => <Tab.Pane><EpisodeList/></Tab.Pane> },
+//   ]
   
-  const TabNav = () => <Tab panes={panes} />
+//   const TabNav = () => <Tab panes={panes} />
   
-  export default TabNav;
+  // export default TabNav;
