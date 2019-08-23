5791275cb9078a7675508550b37579124023a0fa
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-23 08:10:18 -0700

    got yarn installed

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -6,6 +6,15 @@ import { NavLink } from "react-router-dom";
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
 export default function TabNav() {
+    const panes = [
+        { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+        { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+        { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+      ]
+      
+      const TabExampleBasic = () => <Tab panes={panes} />
+      
+      
 
 };
 
